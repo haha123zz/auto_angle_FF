03252197d147d353982bd39472c57d201c6e7f98
haha123zz <86829374+haha123zz@users.noreply.github.com>
2022-01-20 09:35:40 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,24 +1,24 @@
-if not keypress and not keyrelease then game:GetService("Players").LocalPlayer:Kick("Exploit doesn't support required feature") end;
-local UserInputService = game:GetService("UserInputService");
-local Players = game:GetService("Players");
-local Client = Players.LocalPlayer;
-local Character = Client.Character or Client.CharacterAdded:Wait();
-local Humanoid = Character:WaitForChild("Humanoid");
+local UserInputService = game:GetService("UserInputService")
+local Players = game:GetService("Players")
+local Client = Players.LocalPlayer
+local Character = Client.Character or Client.CharacterAdded:Wait()
+local Humanoid = Character:WaitForChild("Humanoid")
 
-local UserInputService = game:GetService("UserInputService");
+local UserInputService = game:GetService("UserInputService")
 UserInputService.InputBegan:Connect(function(input, gpe)
     if input.KeyCode == Enum.KeyCode.Q and not gpe then
-        keypress(0x44); -- d
-        task.wait(.1);
-        keyrelease(0x44);
-        keypress(0x20); -- jump
-        task.wait(.1);
-        keyrelease(0x20);
-        keypress(0x41); -- a 
-        task.wait(.1);
-        keyrelease(0x41);
-        task.wait(.1);
-        keypress(0x45); -- dive
-        keyrelease(0x45);
-    end;
-end);
+        keypress(0x44) -- d
+        task.wait(.2)
+        keyrelease(0x44)
+        keypress(0x20) -- jump
+        mouse1click()
+        task.wait(.1)
+        keyrelease(0x20)
+        keypress(0x41) -- a 
+        task.wait(.1)
+        keyrelease(0x41)
+        task.wait(.1)
+        keypress(0x45) -- dive
+        keyrelease(0x45)
+    end
+end)
